e247386af612834af59cc15a5c87d17935bbb89e
adriana.rodriguez <adriana.rodriguez@opendeusto.es>
2024-11-19 15:50:35 +0100

    formulario

---
+++ static/java/formulario.js
@@ -0,0 +1,7 @@
+
+var formulario = document.getElementById("form");
+formul
+formulario.addEventListener("submit", function (event) {
+      
+    alert("Formulario enviado con éxito. Gracias por tu reserva.");
+});
