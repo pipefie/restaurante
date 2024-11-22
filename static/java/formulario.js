474b86f962f5dc525824710d00679eb7274f309f
adriana.rodriguez <adriana.rodriguez@opendeusto.es>
2024-11-22 13:30:15 +0100

    formulario json

--- static/java/formulario.js
+++ static/java/formulario.js
@@ -1,7 +1,30 @@
 
 var formulario = document.getElementById("form");
-formul
 formulario.addEventListener("submit", function (event) {
       
-    alert("Formulario enviado con éxito. Gracias por tu reserva.");
+    var nombre = document.getElementById("nombre").value;
+    var hora = document.getElementById("hora").value;
+    var fecha = document.getElementById("fecha").value;
+
+    alert(`Formulario enviado con éxito. Gracias por tu reserva, ${nombre}.
+Recuerda que tu reserva es el ${fecha} a las ${hora}. ¡Te esperamos!`);
 });
+
+
+document.getElementById("email").addEventListener("input", function () {
+    const email = this.value;  // aqui tengo el valor del email
+   
+    // Expresión regular para validar correos electrónicos
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mensaje = document.getElementById("mensaje-email");
+
+    if (emailRegex.test(email)) { // esto se utiliza para verificar que la cadena de texto cumple
+        mensaje.textContent = "El correo es válido.";
+        mensaje.style.color = "green";
+    } else {
+        mensaje.textContent = "El correo no es válido.";
+        mensaje.style.color = "red";
+    }
+});
+
+
